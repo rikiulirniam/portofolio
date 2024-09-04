3e003cea5b4a5c4dbcaf16f9c326ebdf5193a948
rikiulirniam <rikiulir@gmail.com>
2024-09-04 16:51:27 +0700

    base

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return (
+        <>
+        <main className="container w-screen h-screen flex flex-col items-center justify-center bg-oxford">
+            <h1>Hello World</h1>
+        </main>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      slate: "#1E293B",
+      oxford: "#0f172a",
+    },
+  },
+  plugins: [],
+};
