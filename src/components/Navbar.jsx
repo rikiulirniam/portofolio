47dc44dcd3196ef5225bc1273d9d8c968ccd6705
rikiulirniam <rikiulir@gmail.com>
2025-01-25 23:43:06 +0700

    animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react";
+import DecryptedText from "./DecryptedText/DecryptedText";
 
 export const Navbar = () => {
   return (
@@ -6,15 +7,30 @@ export const Navbar = () => {
       initial={{ y: -10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="flex items-center justify-center backdrop-blur fixed top-0 left-[50]  py-4 px-10 z-50 w-screen"
-      style={{
-        background: "rgba(0, 0, 0, 0.5)",
-      }}
+      className="flex items-center justify-center backdrop-blur fixed top-0 left-[50] border-solid border-gray border-b-2   py-4 px-10 z-50 w-screen"
+      style={
+        {
+          // background: "rgba(0, 0, 0, 0.5)",
+        }
+      }
     >
       <div className="nav-content flex items-center justify-between container">
         <div className="flex flex-col justify-center ">
           <h1 className=" flex items-center justify-center text-white text-xl font-mono font-bold">
-            <span>Riki</span>
+            <span>
+              <DecryptedText
+                text="Riki"
+                animateOn="hover"
+                revealDirection="start"
+                className="text-white font-bold"
+                encryptedClassName="text-red font-bold"
+                characters="QPALOUCTHENWMowiauqyhtgfvznxml123408975!*&^%$#@?"
+                speed={50}
+                maxIterations={50}
+                // sequential={true}
+              />
+              
+            </span>
           </h1>
         </div>
         <div className="flex items-center justify-center gap-6 font-mono px-4">
@@ -29,8 +45,31 @@ export const Navbar = () => {
             </a>
           </span>
         </div>
-        <div className="social text-white">
-          <h1>miau miau miau</h1>
+        <div className="social flex items-center gap-5 text-white">
+          <a
+            href="rikiulir@gmail.com"
+            className="saturate-0 hover:saturate-100 cursor-pointer w-6 "
+          >
+            <img src="./images/social/linkedin.png" alt="linkedin" />
+          </a>
+          <a
+            href=""
+            className="saturate-0 hover:saturate-100 cursor-pointer w-6 "
+          >
+            <img src="./images/social/instagram.png" alt="instagram" />
+          </a>
+          <a
+            href=""
+            className="saturate-0 hover:saturate-100 cursor-pointer w-6 "
+          >
+            <img src="./images/social/email.png" alt="email" />
+          </a>
+          <a
+            href=""
+            className="saturate-0 hover:saturate-100 cursor-pointer w-6 "
+          >
+            <img src="./images/social/github.png" alt="github" />
+          </a>
         </div>
       </div>
     </motion.nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { motion, useScroll, useSpring } from "motion/react";
 import Swal from "sweetalert2";
 import { Navbar } from "../components/Navbar";
+import DecryptedText from "../components/DecryptedText/DecryptedText";
+import FallingText from "../components/FallingText/FallingText";
 
 export const Home = () => {
   const { scrollYProgress } = useScroll();
@@ -55,32 +57,40 @@ export const Home = () => {
       <Navbar />
       <div className="flex flex-col items-center">
         <div
-          className="bg fixed -z-10 w-screen h-screen"
-          style={{
-            background:
-              "linear-gradient(to bottom left,  rgba(1,2,2,0.8), rgba(0,0,0,0.9), black)",
-          }}
+          className="bg fixed -z-10 w-screen bg-black h-screen"
+          style={
+            {
+              // background:
+              // "linear-gradient(to top right,  rgba(1,2,2,0.8), rgba(0,0,0,0.9), black)",
+            }
+          }
         ></div>
-        <main className="bg-transparent container min-h-screen w-full flex flex-col justify-center align-middle">
+        <section className="bg-transparent container min-h-screen w-full flex flex-col justify-center align-middle">
           <div className="hero-section h-screen w-full flex items-center justify-center  bg-transparent">
             <div className="intro w-1/2 flex flex-col justify-center  ">
               <div className="intro-content w-4/5 flex flex-col justify-center ">
                 <h1 className="text-5xl font-mono">
-                  <Typing rawText="-Hello, my name is Riki Ulir Niam" />
+                  <DecryptedText
+                    text="Hello my name is Riki"
+                    animateOn="view"
+                    revealDirection="start"
+                    className="text-white font-bold"
+                    encryptedClassName="text-red font-bold"
+                    characters="QPALOUCTHENWMowiauqyhtgfvznxml123408975!*&^%$#@?"
+                    speed={50}
+                    maxIter
+                    ations={50}
+                    sequential={true}
+                  />
                 </h1>
 
-                <motion.p
-                  initial={{ x: 0 }}
-                  animate={{ x: 10 }}
-                  transition={{ duration: 0.5 }}
-                  className="text-white text-md my-2"
-                >
+                <p className="text-white text-md my-2">
                   a web developer focused on creating modern and responsive
                   digital solutions!
-                </motion.p>
-                <div className="w-full flex items-center px-3 gap-2">
+                </p>
+                <div className="w-full flex items-center py-2 gap-2">
                   <button
-                    className="bg-white border-white border-2 hover:opacity-75 rounded p-2 px-5"
+                    className="bg-white border-white text-black border-2 hover:opacity-75 rounded p-2 px-5"
                     popovertarget="chooseLangCv"
                     onClick={handleDownloadCV}
                   >
@@ -137,86 +147,43 @@ export const Home = () => {
               </div>
             </div>
           </div>
-          <div className="hero-section h-screen flex md:flex-col-reverse items-center justify-center bg-transparent">
-            <div className="intro w-1/2 flex flex-col justify-center items-center ">
-              <div className="intro-content w-3/5 flex flex-col justify-center items-center ">
-                <h1 className="text-5xl font-mono">
-                  <Typing
-                    rawText="-Hello, my name is Riki"
-                    textColor="black"
-                    blinkColor="red"
-                  />
-                </h1>
-
-                <motion.p
-                  initial={{ x: 0 }}
-                  animate={{ x: 10 }}
-                  transition={{ duration: 0.5 }}
-                  className="text-white text-md my-2"
-                >
-                  a web developer focused on creating modern and responsive
-                  digital solutions!
-                </motion.p>
-                <div className="w-full flex items-center px-3 gap-2">
-                  <button
-                    className="bg-white rounded p-2 px-5"
-                    popovertarget="chooseLangCv"
-                    onClick={handleDownloadCV}
-                  >
-                    Download CV
-                  </button>
-                  <a
-                    className="border-white border-2 rounded text-white  p-2 px-2 flex"
-                    href="/"
-                  >
-                    Learn more{" "}
-                    <span className="p-1">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={2.0}
-                        stroke="currentColor"
-                        className="size-4 rotate-90"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                        />
-                      </svg>
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="img-profile w-1/2 flex items-center justify-center">
-              <div className="home-img w-1/2 rounded-full overflow-hidden relative group">
-                {/* Gambar default */}
-                <img
-                  src="./images/riki1.png"
-                  className="w-full h-full object-cover transition-opacity duration-300 ease-in-out group-hover:opacity-0"
-                  alt="default"
-                />
-                {/* Gambar saat hover */}
-                <motion.img
-                  whileHover={{
-                    opacity: 1,
-                    scale: 1.2,
-                    // transition: { duration: 100 },
-                  }}
-                  src="./images/riki1.jpg"
-                  className="w-full h-full object-cover absolute top-0 left-0 opacity-0"
-                  alt="hovered"
-                />
-              </div>
-            </div>
+        </section>
+        <section className="bg-transparent container h-[70vh]  w-full">
+          <h1 className="text-4xl font-bold  text-center">Skills</h1>
+          <div className="flex items-center justify-center  w-full h-[50vh]">
+            <FallingText    
+              text={` HTML CSS JavaScript React Website Laravel Sanctum TailwindCSS Node.js Git API Integration Responsive`}
+              highlightWords={[
+                "JavaScript",  // Menyoroti keahlian dalam JavaScript
+                "React",  // Menyoroti keahlian dalam React
+                "Web Development",  // Menyoroti bidang keahlian Anda
+                "UI/UX Design",  // Menyoroti keahlian dalam desain antarmuka dan pengalaman pengguna
+                "HTML",  // Menyoroti keahlian dalam HTML
+                "CSS",  // Menyoroti keahlian dalam CSS
+                "TailwindCSS",  // Menyoroti keahlian dalam TailwindCSS
+                "Node.js",  // Menyoroti keahlian dalam Node.js
+                "Git",  // Menyoroti keahlian dalam Git
+                "API Integration",  // Menyoroti keahlian dalam integrasi API
+                "Responsive Design",  // Menyoroti keahlian dalam desain responsif
+              ]}
+              highlightClass="text-red"
+              trigger="scroll"
+              backgroundColor="transparent"
+              wireframes={false}
+              gravity={0.56}
+              fontSize="2rem"
+              mouseConstraintStiffness={0.9}
+            />
           </div>
-        </main>
-        <motion.div
-          className="progress-bar fixed bottom-0 left-0 w-screen h-1 text-left bg-red border-black border-10"
+        </section>
+        {/* <section className="bg-transparent container min-h-screen w-full flex flex-col justify-center align-middle">
+            <h1 className="text-white">Hello World</h1>
+         
+        </section> */}
+        {/* <motion.div
+          className="progress-bar fixed bottom-0 left-0 w-screen h-1 text-left bg-white border-black border-10"
           style={{ scaleX, transformOrigin: "0%" }}
-        />
+        /> */}
       </div>
     </>
   );
