1264be3a351b6745eeaabfb77923b7cd10295de9
rikiulirniam <rikiulir@gmail.com>
2026-02-25 21:58:29 +0700

    style: techstack relayouting

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -9,11 +9,11 @@ import { iconMap } from '../resource/Images'
 export default function TechStack() {
 
   return (
-    <section id='techstack' className='flex justify-center flex-col items-center bg-black text-white'>
+    <section id='techstack' className='flex justify-center flex-col items-center py-14 bg-black text-white'>
       <h1 className='pt-12 pb-8 font-semibold text-3xl underline'>Tech Stack</h1>
 
       {/* Mobile: Swiper */}
-      <div className="w-full px-6 flex justify-center md:hidden pb-14">
+      <div className="w-full px-6 flex justify-center md:hidden ">
         <Swiper
           modules={[Navigation, Autoplay]}
           spaceBetween={20}
@@ -24,12 +24,12 @@ export default function TechStack() {
         >
           {data.teknologi.map((item, index) => (
             <SwiperSlide key={index}>
-              <div className="border p-5 bg-[#111] rounded-md shadow min-h-52 w-full">
-                <div className="flex items-center pb-2">
+              <div className="border p-5 bg-[rgb(17,17,17)] rounded-md shadow  w-full">
+                <div className="flex items-center">
                   <img src={iconMap[item.icon]} width={35} alt={item.name} />
                   <h2 className='text-xl font-medium px-4'>{item.name}</h2>
                 </div>
-                <p className='text-gray-400'>{item.text}</p>
+                {/* <p className='text-gray-400'>{item.text}</p> */}
               </div>
             </SwiperSlide>
           ))}
@@ -40,11 +40,11 @@ export default function TechStack() {
       <div className="hidden md:flex justify-center flex-wrap pb-14 gap-4 px-6">
         {data.teknologi.map((item, index) => (
           <div key={index} className="border p-5 w-72 bg-[#111] rounded-md shadow">
-            <div className="flex items-center pb-2">
+            <div className="flex items-center ">
               <img src={iconMap[item.icon]} width={35} alt={item.name} />
               <h2 className='text-xl font-medium px-4'>{item.name}</h2>
             </div>
-            <p className='text-gray-400'>{item.text}</p>
+            {/* <p className='text-gray-400'>{item.text}</p> */}
           </div>
         ))}
       </div>
